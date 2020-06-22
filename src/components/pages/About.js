6a657fa9cde36f8f358a516a5e117e7aa884b98b
Wilkins Cheng <wilkinscheng@Wilkinss-MacBook-Pro.local>
2020-06-22 17:05:53 +1000

    todoapp with react

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+	return (
+		<React.Fragment>
+			<h1>About</h1>
+			<p>This is the TodoList app v1.8. Coolios!</p>
+		</React.Fragment>
+	);
+}
+
+export default About;
